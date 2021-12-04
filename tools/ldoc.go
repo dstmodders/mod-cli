2f2274f4740c8f0bb04b29c1805e6d7b51634944
Victor Popkov <victor@popkov.me>
2021-12-04 12:41:08 +0200

    Add ldoc in tools package

---
+++ tools/ldoc.go
@@ -0,0 +1,40 @@
+package tools
+
+import (
+	"errors"
+	"strings"
+)
+
+// LDoc represents an LDoc tool.
+type LDoc struct {
+	Tool
+}
+
+// NewLDoc creates a new LDoc instance.
+func NewLDoc() *LDoc {
+	return &LDoc{
+		Tool: *NewTool("LDoc", "ldoc"),
+	}
+}
+
+func (l *LDoc) parseVersion(str string) (string, error) {
+	match := versionRegex.FindString(str)
+	if len(match) == 0 {
+		return "", errors.New("not found")
+	}
+	return strings.TrimSpace(match), nil
+}
+
+// LoadVersion loads an LDoc version.
+func (l *LDoc) LoadVersion() (string, error) {
+	cmd := l.ExecCommand("--version")
+	out, _ := cmd.CombinedOutput()
+
+	ver, err := l.parseVersion(string(out))
+	if err != nil {
+		return ver, err
+	}
+	l.version = ver
+
+	return ver, nil
+}

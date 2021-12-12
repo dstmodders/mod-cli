c8cb2826d54e2e573683f553bbae5d2679ac29d4
Victor Popkov <victor@popkov.me>
2021-12-12 13:43:02 +0200

    Add luacheck in tools package

---
+++ tools/luacheck.go
@@ -0,0 +1,56 @@
+package tools
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"strings"
+)
+
+// Luacheck represents a Luacheck tool.
+type Luacheck struct {
+	Tool
+}
+
+// NewLuacheck creates a new Luacheck instance.
+func NewLuacheck() *Luacheck {
+	return &Luacheck{
+		Tool: *NewTool("Luacheck", "luacheck"),
+	}
+}
+
+func (l *Luacheck) parseVersion(str string) (string, error) {
+	s := strings.Split(str, "\n")
+	if len(s) == 0 {
+		return "", errors.New("not found")
+	}
+	result := s[0]
+	result = strings.ReplaceAll(result, "Luacheck: ", "")
+	result = strings.TrimSpace(result)
+	return result, nil
+}
+
+// LoadVersion loads a Luacheck version.
+func (l *Luacheck) LoadVersion() (string, error) {
+	cmd := l.ExecCommand("--version")
+
+	stdout, _ := cmd.StdoutPipe()
+
+	if err := cmd.Start(); err != nil {
+		return "", err
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 10))
+	_, err := io.Copy(buf, stdout)
+	if err != nil {
+		return "", err
+	}
+
+	ver, err := l.parseVersion(buf.String())
+	if err != nil {
+		return ver, err
+	}
+	l.version = ver
+
+	return ver, nil
+}
